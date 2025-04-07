f2dc5cfca3963f5895cdca4e47ff724a1e6a5b9f
ryo-fujinone <117496712+ryo-fujinone@users.noreply.github.com>
2025-04-08 07:31:29 +0900

    Fixed an issue where an error may occur during playback with a function that Force the highest resolution

--- src/main.js
+++ src/main.js
@@ -1232,7 +1232,7 @@ const runXhook = () => {
           const dom = parser.parseFromString(mpd, "text/xml");
 
           const representations = dom.querySelectorAll(
-            "AdaptationSet[contentType='video'] > Representation"
+            "Period:first-child AdaptationSet[contentType='video'] > Representation"
           );
 
           const highestRepresentation = Array.from(representations).reduce(
