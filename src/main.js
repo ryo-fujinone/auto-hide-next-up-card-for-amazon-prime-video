99723faefb535b432eb8fab5f97199191bfcce27
ryo-fujinone <117496712+ryo-fujinone@users.noreply.github.com>
2025-04-08 18:25:46 +0900

    Bumped version to 2.9.1

--- src/main.js
+++ src/main.js
@@ -27,7 +27,7 @@ const getDefaultOptions = () => {
     disableNextup_xhook: false,
     enableAutoplay_xhook: false,
     forcePlayNextEpisode_xhook: false,
-    scriptVersion: "2.9.0",
+    scriptVersion: "2.9.1",
   };
 };
 
