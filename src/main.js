eddf7a41e6f66958c920834a618f40d899872e65
ryo-fujinone <117496712+ryo-fujinone@users.noreply.github.com>
2025-04-09 14:13:04 +0900

    Bumped version to 2.10.0

--- src/main.js
+++ src/main.js
@@ -27,7 +27,7 @@ const getDefaultOptions = () => {
     removeAdRelatedData: false,
     enableAutoplay_xhook: false,
     forcePlayNextEpisode_xhook: false,
-    scriptVersion: "2.9.1",
+    scriptVersion: "2.10.0",
   };
 };
 
