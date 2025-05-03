7276b0f00158dfb0a004648475a143cdd1dbc046
ryo-fujinone <117496712+ryo-fujinone@users.noreply.github.com>
2025-05-03 11:23:27 +0900

    Fixed an issue where the feature to prevent darkening when next up appears was no longer working

--- src/main.js
+++ src/main.js
@@ -2654,10 +2654,10 @@ class ElementController {
 
     if (!document.querySelector("#preventsDarkeningInConjunctionWithNextup")) {
       const css = `
-        .atvwebplayersdk-overlays-container:has(.atvwebplayersdk-nextupcard-show) >.fkpovp9 {
+        .atvwebplayersdk-player-container:has(.atvwebplayersdk-nextupcard-show) .fkpovp9 {
           opacity: 0 !important;
         }
-        .atvwebplayersdk-overlays-container:has(.atvwebplayersdk-nextupcard-show):has(.f1icw8u[style='cursor: pointer;']) >.fkpovp9 {
+        .atvwebplayersdk-player-container:has(.atvwebplayersdk-nextupcard-show):has(.f1icw8u[style*='cursor: pointer;']) .fkpovp9 {
           opacity: 1 !important;
         }
       `;
